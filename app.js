722b8205faae26e298858618abe2c1b04bc0c48b
EC2 Default User <ec2-user@ip-172-31-7-86.us-west-1.compute.internal>
2020-07-26 21:33:24 +0000

    Robert's V4

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const request = require("request");
+const pool = require("./dbPool.js");
 
-app.engine('html', require('ejs').renderFile);
+app.set("view engine", "ejs");
 app.use(express.static("public"));
+//app.engine('html', require('ejs').renderFile);
 
-//routes 
+//***View routes*** 
 app.get("/", function(req, res){
  res.render("index.ejs");
 });
@@ -29,45 +32,22 @@ app.get("/reports", function(req, res){
  res.render("reports.ejs");
 });
 
-app.get("/api/updateFavorites", function(req, res){
-  let sql;
-  let sqlParams;
-  switch (req.query.action) {
-    case "add": sql = "INSERT INTO favorites (imageUrl, keyword) VALUES (?,?)";
-                sqlParams = [req.query.imageUrl, req.query.keyword];
-                break;
-    case "delete": sql = "DELETE FROM favorites WHERE imageUrl = ?";
-                sqlParams = [req.query.imageUrl];
-                break;
-  }//switch
-  pool.query(sql, sqlParams, function (err, rows, fields) {
-    if (err) throw err;
-    console.log(rows);
-    res.send(rows.affectedRows.toString());
-  });
-});//api/updateFavorites
+//***API Routes*** 
+
+app.get("/api/populateAlbumsArray", function(req, res){
+ 
+ let sql = "SELECT * FROM albums";
+ 
+ pool.query(sql, function(err, rows, fields){
+  if (err) throw err;
+  console.log(rows);
+  res.send(rows);
+  
+ });
+
+});//app.get(populateAlbumArray);
 
 
-app.get("/getKeywords",  function(req, res) {
-  let sql = "SELECT DISTINCT keyword FROM favorites ORDER BY keyword";
-  let imageUrlArray = ["img/favorite.png"];
-  pool.query(sql, function (err, rows, fields) {
-     if (err) throw err;
-     console.log(rows);
-     res.render("favorites", {"imageUrlArray": imageUrlArray, "rows":rows});
-  });  
-});//getKeywords
-
-app.get("/api/getFavorites", function(req, res){
-  let sql = "SELECT imageURL FROM favorites WHERE keyword = ?";
-  let sqlParams = [req.query.keyword];  
-  pool.query(sql, sqlParams, function (err, rows, fields) {
-    if (err) throw err;
-    console.log(rows);
-    res.send(rows);
-  });
-    
-});//api/getFavorites
 
  //start server
  app.listen(process.env.PORT, process.env.IP, function(){
