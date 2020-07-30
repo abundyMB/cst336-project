e267e3c2ec890acb385622b07f09f31b937f981d
EC2 Default User <ec2-user@ip-172-31-7-86.us-west-1.compute.internal>
2020-07-30 09:03:20 +0000

    Added admin login using pre-set admin credentials in database.

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const app = express();
+const session = require('express-session');
 const request = require("request");
+const bcrypt = require('bcrypt');
 const pool = require("./dbPool.js");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 //app.engine('html', require('ejs').renderFile);
+
+app.use(session({
+    secret: "top secret!",
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(express.urlencoded({extended: true}));
 
 //***View routes*** 
 app.get("/", function(req, res){
@@ -32,24 +42,88 @@ app.get("/reports", function(req, res){
  res.render("reports.ejs");
 });
 
+app.get("/signup", function(req, res){
+ res.render("signup.ejs");
+});
+
+app.get("/adminLogin", function(req, res){
+ res.render("adminLogin.ejs");
+});
+
+app.post("/", async function(req, res) {
+    
+    let username = req.body.adminuser;
+    let password = req.body.adminpwd;
+    
+    let result = await checkUsername(username);
+    console.dir(result);
+    let hashedPwd = "";
+    
+    if (result.length > 0) {
+        hashedPwd = result[0].password;
+    }
+    
+    let passwordMatch = await checkPassword(password, hashedPwd);
+    console.log("passwordMatch: " + passwordMatch);
+    
+    if (passwordMatch) {
+        console.log("Password and username Match");
+        req.session.authenticated = true;
+        res.render("index");
+    }
+    else {
+        console.log("No match");
+        res.render("cart"); //, {"loginError":true});
+    }
+});
+
 //***API Routes*** 
 
-app.get("/api/populateAlbumsArray", function(req, res){
+// app.get("/api/populateAlbumsArray", function(req, res){
  
- let sql = "SELECT * FROM albums";
+//  let sql = "SELECT * FROM albums";
  
- pool.query(sql, function(err, rows, fields){
-  if (err) throw err;
-  console.log(rows);
-  res.send(rows);
+//  pool.query(sql, function(err, rows, fields){
+//   if (err) throw err;
+//   console.log(rows);
+//   res.send(rows);
   
- });
+//  });
 
-});//app.get(populateAlbumArray);
+// });//app.get(populateAlbumArray);
 
+//start server
+app.listen(process.env.PORT, process.env.IP, function(){
+ console.log("Express server is running...");
+});
 
+function checkUsername(username) {
+    let sql = "SELECT * FROM admin WHERE username = ? ";
+    return new Promise( function(resolve, reject) {
+       pool.query(sql, [username], function(err, rows, fields) {
+          if (err) throw err;
+          console.log("Rows found: " + rows.length);
+          resolve(rows);
+        });
+    });
+}
 
- //start server
- app.listen(process.env.PORT, process.env.IP, function(){
-  console.log("Express server is running...");
- });
+// Make sure password is valid.
+function checkPassword(password, hashedValue) {
+    return new Promise( function(resolve, reject) {
+       bcrypt.compare(password, hashedValue, function(err, result) {
+         if (err) throw err;
+         console.log("Result: " + result);
+         resolve(result);
+       });
+    });
+}
+
+function isAuthenticated(req, res, next) {
+    if (!req.session.authenticated) {
+        res.redirect("/");
+    }
+    else {
+        next();
+    }
+}
